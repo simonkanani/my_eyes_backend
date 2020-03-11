05c0f535c3c1eabb331d9163baf485020447e034
simonkanani <simon.kanani@hotmail.co.uk>
2020-03-11 19:47:28 +0000

    fixed storage bug

--- my_eyes/storage.py
+++ my_eyes/storage.py
@@ -3,6 +3,6 @@ from storages.backends.azure_storage import AzureStorage
 
 class PublicAzureStorage(AzureStorage):
     account_name = '00672020team18diag'
-    accountkey = 'gHeUIh1I6yH3aTMmLCVBzdMwXFvSZUb6AU2efk3Lvau8rwJzrNpOpdGGJcDo7OWckE7HJRkpTlHcGIY5'
+    accountkey = 'gHeUIh1I6yH3aTMmLCVBzdMwXFvSZUb6AU2efk3Lvau8rwJzrNpOpdGGJcDo7OWckE7HJRkpTlHcGIY5/IzYDg=='
     azure_container = 'my-eyes-container'
     expiration_secs = None
