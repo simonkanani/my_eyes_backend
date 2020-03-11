967d13fe554a29a1fe1206d84bafa13aa716fa44
simonkanani <simon.kanani@hotmail.co.uk>
2020-03-11 18:39:39 +0000

    added requirements.txt

--- my_eyes/urls.py
+++ my_eyes/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path('users/', include('users.urls')),
     path('part_one/', include('part_one.urls')),
     path('part_two/', include('part_two.urls')),
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
 ]
